f2bc8fb98977ef4cc8eb6f177099479a1249d92d
Gittingthk <141226649+Gittingthk@users.noreply.github.com>
2025-07-15 08:23:32 +0900

    배포버전

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -8,10 +11,27 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+
 android {
-    namespace = "com.example.pettapp"
+    namespace = "com.taehun.pettapp"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
+
+    val keystoreProps = Properties().apply {
+        val f = File(rootDir, "key.properties")
+        if (f.exists()) f.inputStream().use(this::load)
+    }
+
+    signingConfigs {
+        create("release") {
+            if (keystoreProps.isNotEmpty()) {
+                storeFile = file(keystoreProps["storeFile"]!!)
+                storePassword = keystoreProps["storePassword"] as String
+                keyAlias = keystoreProps["keyAlias"] as String
+                keyPassword = keystoreProps["keyPassword"] as String
+            }
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +44,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pettapp"
+        applicationId = "com.taehun.pettapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
